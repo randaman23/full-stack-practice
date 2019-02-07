a1ca33fd5a1980e83fb1524bc446627c0fb4b623
randaman23 <randaman234@gmail.com>
2019-02-06 19:31:01 -0700

    login and routes set up

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,32 @@
 import React, { Component } from "react";
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    };
+    this.handleLogin = this.handleLogin.bind(this);
+  }
+
+  handleLogin(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
   render() {
+      console.log(this.state)
     return (
       <div>
-        <h1>Login</h1>
+        <form>
+          <label>Email:</label>
+          <input type="email" name="email" onChange={this.handleLogin} />
+
+          <label>Password:</label>
+          <input type="password" name="password" onChange={this.handleLogin} />
+          <button>Login</button>
+          <button>Register</button>
+        </form>
       </div>
     );
   }
